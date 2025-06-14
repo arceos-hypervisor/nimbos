bfe2168d6724de40b95926ff40abb9c90cd2ae67
aarkegz <aarkegz@gmail.com>
2025-06-14 11:58:12 +0800

    add `DW8250` driver

--- kernel/src/drivers/uart/mod.rs
+++ kernel/src/drivers/uart/mod.rs
@@ -4,6 +4,7 @@ cfg_if! {
         use uart16550 as imp;
     } else if #[cfg(target_arch = "aarch64")] {
         mod pl011;
+        mod dw8250;
         use pl011 as imp;
     } else if #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))] {
         mod riscv;
